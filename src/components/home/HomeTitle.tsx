5a844f053df410a7bc1bad6625ec10b59699c303
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-04-10 02:33:56 +0900

    🎨Design: 키워드 인풋 페이지 리디자인 #26

--- src/components/home/HomeTitle.tsx
+++ src/components/home/HomeTitle.tsx
@@ -4,18 +4,28 @@ interface KeywordTitleProps {
   headText: string;
   highlightText: string;
   tailText: string;
+  etc1?: string;
+  etc2?: string;
 }
 
 const KeywordTitle: React.FC<KeywordTitleProps> = ({
   headText,
   highlightText,
   tailText,
+  etc1,
+  etc2,
 }) => (
-  <div className="mb-2 flex w-full justify-start">
-    <h1 className="mb-4 mt-1 text-4xl font-bold text-white">
-      {headText} <span className="text-[#00FF66]">{highlightText}</span>
-      <br />
+  <div className="mb-0 flex w-full justify-start">
+    <h1 className="mt-1 text-2xl font-bold text-white">
+      {headText} <span className="text-[#80FFB2]">{highlightText}</span>
       {tailText}
+      {etc1 && (
+        <span className="text-[#80FFB2]">
+          <br />
+          {etc1}
+        </span>
+      )}
+      {etc2 && etc2}
     </h1>
   </div>
 );
