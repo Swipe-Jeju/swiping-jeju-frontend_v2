67c1175f289865b8c56c06ab03be3d34275449bb
HuiChan Seo <gmlcks0513@naver.com>
2024-04-12 00:45:48 +0900

    🐛Fix : keyword default 제거

--- src/components/home/HomeKeywordInput.tsx
+++ src/components/home/HomeKeywordInput.tsx
@@ -61,11 +61,9 @@ const HomeKeywordInput: React.FC<HomeKeywordInputProps> = ({
 
     // 상태 업데이트
     setSwipingAlbum(updatedAlbum);
-    log('submitAlbum' + swipingAlbum.mapList);
 
     // 키워드랑 지도 push 하기
-    // const keywords = await handleGetKeyword();
-    const keywords = ['바다', '맛집', '카페'];
+    const keywords = await handleGetKeyword();
 
     // 키워드 토대로 id, hotplaceList 받아오기
     const response = await getCollections({
